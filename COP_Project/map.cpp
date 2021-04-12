f29aa0ee00ad3b2a2324c59ec584233ba9aa4fb3
diya <diya.yang@ufl.edu>
2021-04-12 19:03:34 -0400

    The basic structure of the map is complete. Need to handle hash conflicts

---
+++ COP_Project/map.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T_Key, class T_Value>
+
+class Map
+{
+private:
+    class Node
+    {
+    public:
+        T_Key key;
+        T_Value value;
+        Node *next;
+
+        Node(T_Key key, T_Value value, Node *next)
+        {
+            this->key = key;
+            this->value = value;
+            this->next = next;
+        }
+    };
+    //Constant
+    const float MAX_LOAD_FACTOR = 0.75;
+    // Variable
+    vector<Node*> mapKeys;
+    int mapSize;
+    int mapCapacity;
+    float loadFactor;
+    hash<T_Key> toGenerateHashValue;
+    T_Value defaultValue;
+
+    //T_Key key;
+
+    T_Value value;
+
+public:
+    Map()
+    {
+        mapSize = 0;
+        mapCapacity = 11;
+        loadFactor = mapSize / mapCapacity;
+
+        mapKeys.resize(mapCapacity);
+    }
+
+    void push(T_Key const &key, T_Value const &value)
+    {
+        // size_t Better compatibility than unorder int
+        size_t hashKey = toGenerateHashValue(key);
+
+        size_t mapKey = hashKey % mapCapacity;
+        cout << mapKey << ", " << value << endl;
+        // *add new map value;
+        // if have same mapKey; hash conflict
+        // if (mapKeys[mapKey] == "")
+        // {
+        //     cout << "null" << endl;
+        // }
+
+        Node *node = new Node(key, value, nullptr);
+
+        mapKeys[mapKey] = node;
+        mapSize++;
+        // if (mapKeys[mapKey].empty())
+        // {
+        //     values.push_back(value);
+        //     mapKeys[mapKey] = values;
+        //     mapSize++;
+        // }
+        // else
+        // {
+        //     mapKeys[mapKey].push_back(value);
+        // }
+    }
+
+    void checkWhetherOverLoad()
+    {
+        if (loadFactor > MAX_LOAD_FACTOR)
+        {
+            mapCapacity *= 2;
+            mapKeys.resize(mapCapacity);
+        }
+    }
+
+    void showAll()
+    {
+        cout << "******************" << endl;
+        for (size_t i = 0; i < mapCapacity; i++)
+        {
+            if ( mapKeys[i] != nullptr)
+            {
+                cout << i << ": " << mapKeys[i]->key << endl;
+            } 
+        }
+    }
+};
+
+main()
+{
+    Map<string, int> m;
+    //m.push("qwe", 1);
+    m.push("1", 1);
+    m.push("2", 2);
+    m.push("3", 3);
+    m.push("4", 4);
+    m.push("5", 5);
+    m.push("6", 6);
+    m.push("7", 7);
+    m.push("8", 8);
+    m.push("9", 9);
+    // Map<string, string> m;
+    // m.push("1", "1");
+    // m.push("2", "2");
+    // m.push("3", "3");
+    // m.push("4", "4");
+    // m.push("5", "5");
+    // m.push("6", "6");
+    // m.push("7", "7");
+    // m.push("8", "8");
+    // m.push("9", "9");
+    m.showAll();
+
+    return 0;
+}
---
+++ COP_Project/mapTest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+#include <hash_set>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    cout << v.capacity();
+    v.push_back(1);
+    v.push_back(1);
+v.push_back(1);
+v[3] = 9;
+    cout << " " << v.capacity();
+
+    return 0;
+}
