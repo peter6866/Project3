d4e6386a1f657db369d5fd886dc5838ac801b4e8
Jiayu Huang <Peter.Huang518@outlook.com>
2021-04-17 22:32:08 +0800

    update

--- COP_Project/BST.h
+++ COP_Project/BST.h
@@ -83,12 +83,36 @@ private:
 			TreeNode* temp = s.top();
 			s.pop();
 			//inorder traversal
-			if (temp->right)
-				s.push(temp->right);
-			if (temp->ori_city == ori_city && temp->des_city == des_city && (temp->date / 10000) == year)
-				result.push_back(temp);
 			if (temp->left)
 				s.push(temp->left);
+			if (temp->ori_city == ori_city && temp->des_city == des_city && (temp->date / 10000) == year)
+				result.push_back(temp);
+			if (temp->right)
+				s.push(temp->right);
+		}
+		return result;
+	}
+
+	vector<TreeNode*> findPopulationData(TreeNode* root, const string& des_city, int year)
+	{
+		vector<TreeNode*> result;
+		if (root == nullptr)
+			return result;
+
+		//inorder traversal using stack
+		stack<TreeNode*> s;
+		s.push(root);
+
+		while (!s.empty()) {
+			TreeNode* temp = s.top();
+			s.pop();
+			
+			if (temp->left)
+				s.push(temp->left);
+			if (temp->des_city == des_city && (temp->date / 10000) == year)
+				result.push_back(temp);
+			if (temp->right)
+				s.push(temp->right);
 		}
 		return result;
 	}
@@ -117,33 +141,33 @@ public:
 		//output
 		for (int i = 0; i < v.size(); i++) {
 			if ((v[i]->date / 100) % 100 == 1)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in January: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in January: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 2)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in Febuary: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in Febuary: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 3)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in March: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in March: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 4)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in Apirl: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in Apirl: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 5)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in May: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in May: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 6)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in June: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in June: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 7)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in July: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in July: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 8)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in August: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in August: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 9)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in September: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in September: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 10)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in October: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in October: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 11)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in November: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in November: " << v[i]->flights << endl;
 			if ((v[i]->date / 100) % 100 == 12)
-				cout << "Total flights " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in December: " << v[i]->flights << endl;
+				cout << "Total routes " << "from " << v[i]->ori_city << " to " << v[i]->des_city << " in December: " << v[i]->flights << endl;
 		}
 	}
 
-	void seatOccupancy(string ori_city, string des_city, int year) {
+	void seatOccupancy(const string& ori_city, const string& des_city, int year) {
 		vector<TreeNode*> v = findFlights(root, ori_city, des_city, year);
 
 		int passengerSum = 0;
@@ -159,7 +183,27 @@ public:
 		cout.precision(3);
 		percentage = ((float)passengerSum / (float)seatSum) * 100;
 		cout << "Average seat Occupancy: " << percentage << "%" << endl;
+		cout << endl;
 	}
 
+	pair<int, int> populationTrend(const string& des_city, int year)
+	{
+		vector<TreeNode*> v = findPopulationData(root, des_city, year);
+		pair<int, int> p;
+
+		int passengerSum = 0;
+		int population;
+		for (int i = 0; i < v.size(); i++)
+			passengerSum += v[i]->passengers;
+
+		cout << "People traveled to " << des_city << " in " << year << ": " << passengerSum << endl;
+		cout << "Population of " << des_city << " in " << year << ": " << v[0]->des_population << endl;
+		cout << endl;
+
+		p.first = passengerSum;
+		p.second = v[0]->des_population;
+
+		return p;
+	}
 
 };
--- COP_Project/main.cpp
+++ COP_Project/main.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <sstream>
+#include <istream>
+#include <limits>
 #include "BST.h"
 using namespace std;
 
@@ -44,38 +46,74 @@ int main() {
 	airport.searchDate(20000101);
 	cout << "finish searching" << endl;*/
 
-	//function 1 test
-	//user input origin_city, des_city, and year
-	cout << "Find flights" << endl;
-	airport.findFlights("Aberdeen SD", "Minneapolis MN", 2000);
+	int selection;
+	cout << "Select Function" << endl;
+	cout << "1. Number of Routes" << endl;
+	cout << "2. Seat Occupancy" << endl;
+	cout << "3. Population Trend" << endl;
+	cout << "4. Quit" << endl;
+	cout << "Select: ";
+	cin >> selection;
 	cout << endl;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-	//function 2 test
-	//string ori_city, des_city, year;
-	cout << "Seat Occupancy" << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2000);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2001);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2002);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2003);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2004);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2005);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2006);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2007);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2008);
-	cout << endl;
-	airport.seatOccupancy("Aberdeen SD", "Minneapolis MN", 2009);
-	cout << endl;
-	//ÓÐÇ÷ÊÆ
-	
-	airport.seatOccupancy("Aberdeen SD", "Pierre SD", 2000);
+	while (selection != 4)
+	{
+		if (selection == 1)
+		{
+			//Aberdeen SD, Minneapolis MN, 2000
+			string inputOriCity, inputDestCity;
+			int inputYear;
+
+			cout << "Origin City: ";
+			getline(cin, inputOriCity);
+			cout << "Destination City: ";
+			getline(cin, inputDestCity);
+			cout << "Year: ";
+			cin >> inputYear;
+
+			//function 1 test
+			//user input origin_city, des_city, and year
+			airport.findFlights(inputOriCity, inputDestCity, inputYear);
+			cout << endl;
+		}
+		else if (selection == 2)
+		{
+			//function 2 test
+			//string ori_city, des_city, year;
+			string inputOriCity, inputDestCity;
+
+			cout << "Origin City: ";
+			getline(cin, inputOriCity);
+			cout << "Destination City: ";
+			getline(cin, inputDestCity);
+
+			for (int i = 2000; i <= 2009; i++)
+				airport.seatOccupancy(inputOriCity, inputDestCity, i);
+
+		}
+		else if (selection == 3)
+		{
+			string inputDestCity;
+			cout << "City Name: ";
+			getline(cin, inputDestCity);
+			//X-axis: 2000 to 2009
+			//store Y-axis in a vector
+			vector<int> passengerNum;
+			vector<int> populationNum;
+
+			for (int i = 2000; i <= 2009; i++)
+			{
+				passengerNum.push_back(airport.populationTrend(inputDestCity, i).first);
+				populationNum.push_back(airport.populationTrend(inputDestCity, i).second);
+			}
+				
+		}
+		cout << "Selection: ";
+		cin >> selection;
+	}
+
+	cout << "Program Terminates" << endl;
 	
 	return 0;
 }
